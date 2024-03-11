eb1402ac25195fb5563b154a4c9884bada27b7db
Etwashoeren <sungmin0609@nate.com>
2024-03-11 13:10:55 +0900

    update

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    userId = forms.CharField(min_length=3)
-    password = forms.CharField(min_length=4)
+    userId = forms.CharField(
+        min_length=3,
+        widget=forms.TextInput(
+            attrs={"placeholder": "전화번호, 사용자 이름 또는 이메일"},
+        ),
+    )
+    password = forms.CharField(
+        min_length=4,
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "비밀번호"},
+        ),
+    )
--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from users.forms import LoginForm
 
-
 # Create your views here.
 def login_view(request):
     # 이미 로그인 되어 있다면
@@ -10,23 +9,40 @@ def login_view(request):
         return redirect("/posts/feeds/")
 
     if request.method == "POST":
-        # LoginForm에 인스턴스를 만들며, 입력 데이터는 request.POST를 사용
         form = LoginForm(data=request.POST)
+        # LoginForm에 전달된 데이터가 유효하다면
+        if form.is_valid():
+            # userId와 password 값을 가져와 변수에 할당
+            userId = form.cleaned_data["userId"]
+            password = form.cleaned_data["password"]
 
-        # LoginForm에 들어온 데이터가 적절한지 유효성 검사
-        print("form.is_valid()", form.is_valid())
+            # userId, password에 해당하는 사용자가 있는지 검사
+            user = authenticate(userId=userId, password=password)
 
-        # 유효성 검사 이후에는 cleand_data에서 데이터를 가져와 사용
-        print("form.cleaned_data:", form.cleaned_data)
+            # 해당 사용자가 존재한다면
+            if user:
+                # 로그인 처리 후, 피드 페이지로 redirect
+                login(request, user)
+                return redirect("/posts/feeds/")
+            # 사용자가 없다면 form에 에러 추가
+            else:
+                form.add_error(None, "입력한 자격증명에 해당하는 사용자가 없습니다")
+
+        # 어떤 경우든 실패한 경우(데이터 검증, 사용자 검사) 다시 LoginForm을 사용한 로그인 페이지 랜더링
         context = {
             "form": form,
         }
         return render(request, 'users/login.html', context)
     else:
-        # LoginForm 인스턴스를 생성
         form = LoginForm()
-        # 생성한 LoginForm 인스턴스를 템플릿에 "form"이라는 키로 전달
         context = {
             "form": form,
         }
         return render(request, 'users/login.html', context)
+
+def logout_view(request):
+    # logout 함수 호출에 request를 전달한다
+    logout(request)
+
+    # logout 처리 후, 로그인 페이지로 이동한다
+    return redirect("/users/login/")
